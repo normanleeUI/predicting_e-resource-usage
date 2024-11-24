46c4e4fee4286faf5242400ef4f2507dc4c4c894
normanleeUI <normanlee@uidaho.edu>
2024-11-24 17:56:38 -0500

    wrote "clean pub year column" section and started on "assign fiscal years" section

--- code/scripts/modularize-me-later.R
+++ code/scripts/modularize-me-later.R
@@ -3,6 +3,8 @@ library(tidyverse)
 library(DBI)
 library(readxl)
 library(cli)
+library(lubridate)
+library(stringr)
 
 # load raw data ####
 az_database_list <- read.csv('raw_data/az_database_list.csv')
@@ -70,10 +72,16 @@ spring_enrollment <- select(spring_enrollment,
                             enrollment = "Count (Data suppression) 1")
 
 grant_funding <- select(grant_funding,
-                        "fiscal-year",
+                        fiscal_year = "fiscal-year",
                         "college",
                         grant_proposals = "Number of proposals",
                         grant_funding = "research expenditures")
+
+platform_usage <- select(platform_usage,
+                         fiscal_year = "Usage.Date.Fiscal.Year",
+                         platform = "Normalized.Platform",
+                         usage = "Usage.Measures.Total",
+                         unique_titles = "Title.Identifier.Count")
 
 ## concatenate "organization.parent.unit.name" columns and drop them in addition to removing/renaming ####
 
@@ -83,3 +91,82 @@ research_outputs <- research_outputs %>%
          pub_year = "Asset.Published.Date..String.",
          type = "Asset.Type",
          unit_affiliations)
+
+# sum enrollment numbers ####
+
+spring_enrollment_sums <- spring_enrollment %>% 
+  group_by(term, college) %>% 
+  summarize(total_enrollment = sum(enrollment, na.rm = TRUE))
+
+view(spring_enrollment_sums)
+
+fall_enrollment_sums <- fall_enrollment %>% 
+  group_by(term, college) %>% 
+  summarize(total_enrollment = sum(enrollment, na.rm = TRUE))
+
+view(fall_enrollment_sums)
+
+# count research outputs by academic unit and fiscal year ####
+
+## clean pub_year column ####
+# cases:
+# yyyy | possible regular expression: ^\d{4}$
+# NULL and ""
+# dd/mm/yyyy (doesn't always include leading 0's) | ^([1-9]|[12][0-9]|3[01])/([1-9]|1[12])/(19|20)\d{2}$
+# mm/dd/yyyy (doesn't always include leading 0's) | ^([1-9]|1[12])/([1-9]|[12][0-9]|3[01])/(19|20)\d{2}$
+# yy-mmm (3 alphanumeric characters for month abbreviation; doesn't always inclued leading 0's) | ^\\d{1,2}-[a-zA-Z]{3}$
+# mmm-yy (3 alphanumeric characters for month abbreviation; doesn't always inclued leading 0's) | ^[a-zA-Z]{3}-\\d{1,2}$
+
+
+
+research_outputs <- research_outputs %>% 
+  mutate(
+    pub_year_cleaned = case_when(
+      str_detect(pub_year, "^\\d{4}$") ~ mdy(paste0('01/01/',pub_year)),
+    
+      str_detect(pub_year, "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(\\d{4}$)") ~ dmy(pub_year),
+    
+      str_detect(pub_year, "^([1-9]|1[12])/([1-9]|[12][0-9]|3[01])/(19|20)\\d{2}$") ~ mdy(pub_year),
+    
+      str_detect(pub_year, "^\\d{1,2}-[a-zA-Z]{3}$") ~ ymd(
+        paste0(
+          str_replace(str_extract(pub_year, "\\d{1,2}"),
+                          "\\d{1,2}",
+                          paste0('20',str_extract(pub_year, "\\d{1,2}"),'-',str_extract(pub_year, '[a-zA-Z]{3}'),'-','01')))),
+    
+      str_detect(pub_year, "^[a-zA-Z]{3}-\\d{1,2}$") ~ ymd(
+        paste0(
+          str_replace(str_extract(pub_year, "\\d{1,2}"),
+                      "\\d{1,2}",
+                      paste0('20',str_extract(pub_year, "\\d{1,2}"),'-',str_extract(pub_year, '[a-zA-Z]{3}'),'-','01')))),
+    
+    TRUE ~ as_date(pub_year),
+    
+  ),
+  
+  needs_fy_sim = case_when(
+    str_detect(pub_year, "^\\d{4}$") ~ TRUE,
+    str_detect(pub_year, "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(\\d{4}$)") ~ FALSE,
+    str_detect(pub_year, "^([1-9]|1[12])/([1-9]|[12][0-9]|3[01])/(19|20)\\d{2}$") ~ FALSE,
+    str_detect(pub_year, "^\\d{1,2}-[a-zA-Z]{3}$") ~ FALSE,
+    str_detect(pub_year, "^[a-zA-Z]{3}-\\d{1,2}$") ~ FALSE
+  )
+)
+
+
+## assign fiscal years; randomly generate if necessary ####
+
+research_outputs <- research_outputs %>% 
+  mutate(fiscal_year = case_when(
+    needs_fy_sim == TRUE ~ case_when(
+      sample(1:2, n(), replace = TRUE) == 1 ~ as.integer(pub_year),
+      sample(1:2, n(), replace = TRUE) == 2 ~ as.integer(pub_year)+1
+    ),
+    
+    
+  ))
+
+view(research_outputs)
+
+## add a modifier that divides the "effort" of the publication by the number of unique academic units listed as authors
+
